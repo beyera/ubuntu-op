93fbb16a461391d451c67aa0dd903eea97b1ca36
Andrew Beyer <beyer@1password.com>
2021-05-08 16:01:44 -0600

    Fixed microsoft edge install for real this time 🤞

--- scripts/config.sh
+++ scripts/config.sh
@@ -57,9 +57,9 @@ function install_brave() {
 # Install Microsoft Edge (beta)
 function install_edge() {
     # gpg key
-    curl -s https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    curl -s https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
     # deb repo
-    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" | sudo tee /etc/apt/sources.list.d/microsoft.list
     # install
     apt update
     sudo apt install -y microsoft-edge-beta
