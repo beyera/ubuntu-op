fc5f92f64ae6d8b5194aa5047efd036b28f86b81
Andrew Beyer <beyer@1password.com>
2021-05-08 13:22:46 -0600

    refactoring + docs

--- README.md
+++ README.md
@@ -1,8 +1,33 @@
 # Ubuntu OP
 
-## Summary
-
 A custom-built version of Ubuntu with 1Password for Linux and 1Password officially supported browsers preinstalled. This aids in testing via a reproducible environment as well as a quick way for folks not familiar with Linux to try 1Password for Linux.
+
+## What is included?
+
+- Ubuntu LTS
+-
+-
+-
+-
+
+## What would I want this for?
+
+- Testing 1Password in a clean and reproducible environnement.
+- To try out 1Password for Linux without needing to fuss with how to install applications on Linux.
+- You need a quick bootstrap to running 1Password in a VM.
+
+## Why did you make it?
+
+A few reasons:
+
+- I came across someone who did a lot of the hard work creating the underlying scripts which quickly create a new ubuntu live iso.
+- Github has actions now which are completely free public repositories. With a little bit of automation magic I now have an up-to-date version of Ubuntu with the latest version of 1Password and supported browsers.
+- I couldn't sleep because my brain was racing about how cool this was.
+
+## Todo
+
+- Create SHA256 checksum and sign it using GPG on every iso creation.
+- Create a sweet landing/download page. In the meantime, you can grab the latest version here:
 
 ## Authors
 
--- scripts/default_config.sh
+++ scripts/default_config.sh
@@ -32,99 +32,27 @@ export TARGET_PACKAGE_REMOVE="
     os-prober \
 "
 
-# Install 1Password for Linux (beta)
-function install_1password() {
-    # gpg key
-    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
-    # deb repo
-    echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 beta main' | sudo tee /etc/apt/sources.list.d/1password.list
-    # debsig-verify policy
-    sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-    curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-    curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-    # install
-    apt update
-    apt install -y 1password
-}
-
-# Install Brave
-function install_brave() {
-    # gpg key
-    curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-    # deb repo
-    echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    # install
-    apt update
-    apt install -y brave-browser
-}
-
-# Install Microsoft Edge (beta)
-function install_edge() {
-    # gpg key
-    curl -s https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-    # deb repo
-    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
-    # install
-    apt update
-    sudo apt install microsoft-edge-dev
-}
-
-# Install Google Chrome (beta)
-function install_google_chrome() {
-    # gpg key
-    curl -s https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-    # deb repo
-    echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
-    #install
-    apt update
-    apt install -y google-chrome-stable
-}
-
-# Install b5x in Chrome (beta), Edge (stable), Firefox  (beta)
-function install_b5x() {
-    # chrome
-    mkdir -p /opt/google/chrome/extensions
-    echo '{"external_update_url":"https://clients2.google.com/service/update2/crx"}' | sudo tee /opt/google/chrome/extensions/khgocmkkpikpnmmkgmdnfckapcdkgfaf.json
-    # edge
-    mkdir -p /usr/share/microsoft-edge/extensions
-    echo '{"external_update_url":"https://edge.microsoft.com/extensionwebstorebase/v1/crx"}' | sudo tee /usr/share/microsoft-edge/extensions/dppgmdbiimibapkepcbdbmkaabgiofem.json
-    # firefox
-    mkdir -p /usr/lib/firefox/distribution
-    echo '{"policies":{"Extensions":{"Install":["https://s3.amazonaws.com/com.1password.b5x/firefox/beta/latest.xpi"]}}}' | sudo tee /usr/lib/firefox/distribution/policies.json
-}
-
 # Package customisation function.  Update this function to customize packages
 # present on the installed system.
 function customize_image() {
     # install graphics and desktop
-    apt install -y \
+    apt-get install -y \
         plymouth-theme-ubuntu-logo \
         ubuntu-gnome-desktop \
         ubuntu-gnome-wallpapers
 
     # useful tools
-    apt install -y \
+    apt-get install -y \
         clamav-daemon \
         terminator \
         apt-transport-https \
-        software-properties-common \
         curl \
         vim \
         nano \
         less
 
-    # install 1password
-    install_1password
-
-    # install browsers
-    apt install -y firefox
-    install_brave
-    install_google_chrome
-    install_b5x
-
     # purge
-    apt purge -y \
+    apt-get purge -y \
         transmission-gtk \
         transmission-common \
         gnome-mahjongg \
