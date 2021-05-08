6dfad71c116bdc2eb181040304ccace3465f341d
Andrew Beyer <beyer@1password.com>
2021-05-08 11:52:13 -0600

    Updated deploy, and added edge, google chrome, b5x to installs

--- scripts/default_config.sh
+++ scripts/default_config.sh
@@ -32,6 +32,7 @@ export TARGET_PACKAGE_REMOVE="
     os-prober \
 "
 
+# Install 1Password for Linux (beta)
 function install_1password() {
     # gpg key
     curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
@@ -43,31 +44,67 @@ function install_1password() {
     sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
     curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
     # install
-    apt-get update
-    apt-get install -y 1password
+    apt update
+    apt install -y 1password
 }
 
-function insall_brave() {
+# Install Brave
+function install_brave() {
     # gpg key
     curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
     # deb repo
     echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
     # install
-    apt-get update
-    apt-get install -y brave-browser
+    apt update
+    apt install -y brave-browser
+}
+
+# Install Microsoft Edge (beta)
+function install_edge() {
+    # gpg key
+    curl -s https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    # deb repo
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
+    # install
+    apt update
+    sudo apt install microsoft-edge-dev
+}
+
+# Install Google Chrome (beta)
+function install_google_chrome() {
+    # gpg key
+    curl -s https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    # deb repo
+    echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
+    #install
+    apt update
+    apt install -y google-chrome-stable
+}
+
+# Install b5x in Chrome (beta), Edge (stable), Firefox  (beta)
+function install_b5x() {
+    # chrome
+    mkdir -p /opt/google/chrome/extensions
+    echo '{"external_update_url":"https://clients2.google.com/service/update2/crx"}' | sudo tee /opt/google/chrome/extensions/khgocmkkpikpnmmkgmdnfckapcdkgfaf.json
+    # edge
+    mkdir -p /usr/share/microsoft-edge/extensions
+    echo '{"external_update_url":"https://edge.microsoft.com/extensionwebstorebase/v1/crx"}' | sudo tee /usr/share/microsoft-edge/extensions/dppgmdbiimibapkepcbdbmkaabgiofem.json
+    # firefox
+    mkdir -p /usr/lib/firefox/distribution
+    echo '{"policies":{"Extensions":{"Install":["https://s3.amazonaws.com/com.1password.b5x/firefox/beta/latest.xpi"]}}}' | sudo tee /usr/lib/firefox/distribution/policies.json
 }
 
 # Package customisation function.  Update this function to customize packages
 # present on the installed system.
 function customize_image() {
     # install graphics and desktop
-    apt-get install -y \
+    apt install -y \
         plymouth-theme-ubuntu-logo \
         ubuntu-gnome-desktop \
         ubuntu-gnome-wallpapers
 
     # useful tools
-    apt-get install -y \
+    apt install -y \
         clamav-daemon \
         terminator \
         apt-transport-https \
@@ -81,10 +118,13 @@ function customize_image() {
     install_1password
 
     # install browsers
+    apt install -y firefox
     install_brave
+    install_google_chrome
+    install_b5x
 
     # purge
-    apt-get purge -y \
+    apt purge -y \
         transmission-gtk \
         transmission-common \
         gnome-mahjongg \
